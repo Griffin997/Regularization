a357e160448e15318c6cb3318770d87af6255d33
Griffin Hampton <griffinhampton@gmail.com>
2023-01-26 13:44:17 -0500

    Comparison with MB results

--- MB_References/BLSA_1742_04_MCIAD_m41/Relaxometry_mapping_NLLS_sliceFocus.m
+++ MB_References/BLSA_1742_04_MCIAD_m41/Relaxometry_mapping_NLLS_sliceFocus.m
@@ -30,28 +30,74 @@ end
 %%
 
 figure;
-subplot(131);imagesc(MWF_NESMA(:,:),[0 0.4]);colormap jet; axis off;colorbar;title('MWF (n.u.)');
-subplot(132);imagesc(T2s_NESMA(:,:),[0 60]);colormap jet; axis off;colorbar;title('T2s (ms)');
-subplot(133);imagesc(T2l_NESMA(:,:),[0 140]);colormap jet; axis off;colorbar;title('T2l (ms)');
-sgtitle(strcat("NESMA Data - m41 - slice 5"))
+subplot(131);imagesc(MWF_slice(:,:),[0 0.4]);colormap jet; axis off;colorbar;title('MWF (n.u.)');
+subplot(132);imagesc(T2s_slice(:,:),[0 60]);colormap jet; axis off;colorbar;title('T2s (ms)');
+subplot(133);imagesc(T2l_slice(:,:),[0 140]);colormap jet; axis off;colorbar;title('T2l (ms)');
+sgtitle(strcat("Processed Slice Data - m41 - slice 5"))
 
 %% 
 
 m41_data = struct();
-m41_data.NESMA.T2l = T2l_NESMA;
-m41_data.NESMA.T2s = T2s_NESMA;
-m41_data.NESMA.MWF = MWF_NESMA;
+m41_data.slice.T2l = T2l_slice;
+m41_data.slice.T2s = T2s_slice;
+m41_data.slice.MWF = MWF_slice;
 
 save('m41_dataStruct_slice5.mat', '-struct', 'm41_data');
 
 %%
 
 load("m41_dataStruct_slice5.mat")
+T2l_slice = slice.T2l;
+T2s_slice = slice.T2s;
+MWF_slice = slice.MWF;
+
+%% MB Results
+
+load("m41_dataStruct.mat")
 T2l_NESMA = NESMA.T2l;
 T2s_NESMA = NESMA.T2s;
 MWF_NESMA = NESMA.MWF;
 
-% T2l_raw = raw.T2l;
-% T2s_raw = raw.T2s;
-% MWF_raw = raw.MWF;
+T2l_raw = raw.T2l;
+T2s_raw = raw.T2s;
+MWF_raw = raw.MWF;
 
+%% Comparison Figures
+
+s = 5;
+figure;
+subplot(131);imagesc(MWF_slice(:,:) - MWF_NESMA(:,:,s),[-0.4 0.4]);colormap jet; axis off;colorbar;title('MWF (n.u.)');
+subplot(132);imagesc(T2s_slice(:,:) - T2s_NESMA(:,:,s),[-60 60]);colormap jet; axis off;colorbar;title('T2s (ms)');
+subplot(133);imagesc(T2l_slice(:,:) - T2l_NESMA(:,:,s),[-140 140]);colormap jet; axis off;colorbar;title('T2l (ms)');
+sgtitle(strcat("NESMA Difference Data - m41 - slice 5"))
+
+figure;
+subplot(131);imagesc(MWF_slice(:,:) - MWF_raw(:,:,s),[-0.4 0.4]);colormap jet; axis off;colorbar;title('MWF (n.u.)');
+subplot(132);imagesc(T2s_slice(:,:) - T2s_raw(:,:,s),[-60 60]);colormap jet; axis off;colorbar;title('T2s (ms)');
+subplot(133);imagesc(T2l_slice(:,:) - T2l_raw(:,:,s),[-140 140]);colormap jet; axis off;colorbar;title('T2l (ms)');
+sgtitle(strcat("Raw Difference Data - m41 - slice 5"))
+
+%%
+
+load I4D_NESMA;
+load I4D_raw;
+
+%%
+
+figure;
+imagesc(slice_oi(:,:,1) - I4D_NESMA(:,:,s,1));colormap jet; axis off;colorbar;
+sgtitle(strcat("NESMA Difference Signals - m41 - slice 5"))
+
+figure;
+imagesc(slice_oi(:,:,1) - I4D_raw(:,:,s,1));colormap jet; axis off;colorbar;
+sgtitle(strcat("Raw Difference Signals - m41 - slice 5"))
+
+%%
+
+figure;
+imagesc(abs(slice_oi(:,:,1) - I4D_NESMA(:,:,s,1))>0);colormap jet; axis off;colorbar;
+sgtitle(strcat("NESMA Difference Signals Location - m41 - slice 5"))
+
+figure;
+imagesc(abs(slice_oi(:,:,1) - I4D_raw(:,:,s,1))>0);colormap jet; axis off;colorbar;
+sgtitle(strcat("Raw Difference Signals Location - m41 - slice 5"))
