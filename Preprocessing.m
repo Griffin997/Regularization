ff9e065b21b4ea0d5e3b8c690e8343b5e61c11ea
Griffin Hampton NIA Laptop <griffinhampton@gmail.com>
2023-01-26 14:03:06 -0500

    Added new slices

--- Preprocessing.m
+++ Preprocessing.m
@@ -4,7 +4,7 @@ directory_name = "MB_References";
 file_name = "BLSA_1742_04_MCIAD_m41";
 raw_name = "I4D_NESMA.mat";
 load(strcat(directory_name,"\",file_name,'\', raw_name));
-save_file = false;
+save_file = true;
 
 I4D_object = I4D_NESMA;
 
@@ -12,14 +12,14 @@ for i = 1:size(I4D_object,3)
     I_slice = I4D_object(:,:,i,1);
     figure;
     imagesc(I_slice)
-    title(strcat("I_{raw} data on slice = ",string(i)))
+    title(strcat("I_{NESMA} data on slice = ",string(i)))
 end
 
 
 %% Process Section
 close all
 
-slice_focus = 5;
+slice_focus = 3;
 figure
 im = imagesc(I4D_object(:,:,slice_focus,1));
 area_selected = drawfreehand;
@@ -34,10 +34,10 @@ end
 %% Save Section
 if save_file
 
-    save(strcat(directory_name,'/',file_name,'/','rS_slice',string(slice_focus),'.mat'), 'slice_oi')
+    save(strcat(directory_name,'/',file_name,'/','NESMA_slice',string(slice_focus),'.mat'), 'slice_oi')
 
 end
 %% Load Section Check
-load(strcat(directory_name,'/',file_name,'/','rS_slice',string(slice_focus),'.mat'))
+load(strcat(directory_name,'/',file_name,'/','NESMA_slice',string(slice_focus),'.mat'))
 
 imagesc(slice_oi(:,:,32))
